6592c5624043415b38ba2c0adbb63f16cdd28d71
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-03-22 00:41:21 +0100

    Added progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
 use rimage::{decoders, encoders, Config};
 
 fn main() {
     let conf = Config::parse_from(wild::args_os());
+    let pb = ProgressBar::new(conf.input.len() as u64);
+    pb.set_style(
+        ProgressStyle::with_template("{bar:40.green/blue}  {pos}/{len} {wide_msg}")
+            .unwrap()
+            .progress_chars("##*"),
+    );
+    pb.set_position(0);
 
     for path in conf.input {
+        pb.set_message(path.file_name().unwrap().to_str().unwrap().to_owned());
+        pb.inc(1);
+
         let (pixels, width, height) = decoders::decode_image(&path).unwrap();
 
         encoders::encode_image(
@@ -17,4 +28,5 @@ fn main() {
         )
         .unwrap();
     }
+    pb.finish();
 }
