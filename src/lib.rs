f882c1cff90b05e6281eb56649bad833e9cdd88a
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-03-21 01:21:23 +0100

    Added code to process images

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Config {
+    /// Input files to be compressed
+    pub input: Vec<std::path::PathBuf>,
+
+    /// Quality of output images from 0 to 1
+    #[arg(short, long, default_value_t = 0.75)]
+    pub quality: f32,
+}
+
+pub mod encoders;
+pub mod decoders;
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use clap::Parser;
-
-#[derive(Parser, Debug)]
-struct Args {
-    /// Input files to be compressed
-    input: Vec<std::path::PathBuf>,
-
-    /// Quality of output images from 0 to 1
-    #[arg(short, long, default_value_t = 0.75)]
-    quality: f32,
-}
+use rimage::{decoders, encoders, Config};
 
 fn main() {
-    let args = Args::parse_from(wild::args_os());
+    let conf = Config::parse_from(wild::args_os());
 
-    println!("Args: {args:#?}");
+    for path in conf.input {
+        let (pixels, width, height) = decoders::decode_image(&path).unwrap();
+
+        encoders::encode_image(&path, &pixels, "png", width, height, conf.quality).unwrap();
+    }
 }
