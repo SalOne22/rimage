3c2af4b533e01bede6bacf04c7f19ef95e5e400a
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-03-23 12:38:24 +0100

    Added parallel processing, functions now return only io::Error, added log messages and log output

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
+use log::info;
+use rayon::{prelude::*, ThreadPoolBuilder};
 use rimage::{decoders, encoders, Config};
 
 fn main() {
+    pretty_env_logger::init();
     let conf = Config::parse_from(wild::args_os());
     let pb = ProgressBar::new(conf.input.len() as u64);
     pb.set_style(
-        ProgressStyle::with_template("{bar:40.green/blue}  {pos}/{len} {wide_msg}")
+        ProgressStyle::with_template("{bar:40.green/blue}  {pos}/{len}")
             .unwrap()
             .progress_chars("##*"),
     );
     pb.set_position(0);
 
-    for path in conf.input {
-        pb.set_message(path.file_name().unwrap().to_str().unwrap().to_owned());
-        pb.inc(1);
+    let pool = ThreadPoolBuilder::new().build().unwrap();
 
-        let (pixels, width, height) = decoders::decode_image(&path).unwrap();
+    pool.install(|| {
+        conf.input.par_iter().for_each(|path| {
+            info!("Processing {path:?}");
+            let (pixels, width, height) = decoders::decode_image(path).unwrap();
 
-        encoders::encode_image(
-            &path,
-            &pixels,
-            &conf.output_format,
-            width,
-            height,
-            conf.quality,
-        )
-        .unwrap();
-    }
+            encoders::encode_image(
+                path,
+                &pixels,
+                &conf.output_format,
+                width,
+                height,
+                conf.quality,
+            )
+            .unwrap();
+
+            pb.inc(1);
+        })
+    });
+
     pb.finish();
 }
