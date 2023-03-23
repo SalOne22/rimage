e1cd4a394fe773f5f151494778d6f948cd025929
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-03-23 12:38:24 +0100

    Added parallel processing, functions now return only io::Error, added log messages and log output

--- src/encoders.rs
+++ src/encoders.rs
@@ -1,9 +1,10 @@
 use std::{
-    error, fs,
+    fs,
     io::{self, BufWriter, Cursor},
     panic, path,
 };
 
+use log::{debug, info};
 use mozjpeg::Compress;
 use rgb::{ComponentBytes, RGBA8};
 
@@ -26,12 +27,13 @@ pub fn encode_image(
     height: usize,
     quality: f32,
 ) -> Result<(), io::Error> {
+    info!("Started encoding...");
     let file_bytes = match output_format {
-        "jpg" => encode_jpeg(pixels, width, height, quality),
-        "png" => encode_png(pixels, width, height),
+        "jpg" | "jpeg" => encode_jpeg(pixels, width, height, quality)?,
+        "png" => encode_png(pixels, width, height)?,
         _ => return Err(io::Error::new(io::ErrorKind::Other, "File not supported")),
-    }
-    .unwrap();
+    };
+    info!("Encoded {} bytes", file_bytes.len());
 
     let mut path = path.to_owned();
     path.set_extension(output_format);
@@ -43,8 +45,8 @@ fn encode_jpeg(
     width: usize,
     height: usize,
     quality: f32,
-) -> Result<Vec<u8>, Box<dyn error::Error>> {
-    panic::catch_unwind(|| -> Result<Vec<u8>, Box<dyn error::Error>> {
+) -> Result<Vec<u8>, io::Error> {
+    panic::catch_unwind(|| -> Result<Vec<u8>, io::Error> {
         let mut comp = Compress::new(mozjpeg::ColorSpace::JCS_EXT_RGBA);
 
         comp.set_size(width, height);
@@ -58,19 +60,16 @@ fn encode_jpeg(
         comp.finish_compress();
         Ok(comp.data_to_vec().unwrap())
     })
-    .unwrap_or(Err(Box::new(io::Error::new(
+    .unwrap_or(Err(io::Error::new(
         io::ErrorKind::InvalidData,
         "Failed to read jpeg",
-    ))))
+    )))
 }
 
-fn encode_png(
-    pixels: &[RGBA8],
-    width: usize,
-    height: usize,
-) -> Result<Vec<u8>, Box<dyn error::Error>> {
+fn encode_png(pixels: &[RGBA8], width: usize, height: usize) -> Result<Vec<u8>, io::Error> {
     let mut buf = Cursor::new(Vec::with_capacity(width * height * 4));
     {
+        debug!("Started encoding png...");
         let w = &mut BufWriter::new(&mut buf);
         let mut encoder = png::Encoder::new(w, width as u32, height as u32);
         encoder.set_color(png::ColorType::Rgba);
@@ -79,12 +78,19 @@ fn encode_png(
         w.write_image_data(pixels.as_bytes())?;
         w.finish()?;
     }
+
+    debug!("Encoded {} bytes", buf.get_ref().len());
     let opts = oxipng::Options::from_preset(2);
-    Ok(oxipng::optimize_from_memory(buf.get_ref(), &opts)?)
+
+    let data =
+        oxipng::optimize_from_memory(buf.get_ref(), &opts).unwrap_or(buf.get_ref().to_owned());
+    Ok(data)
 }
 
 #[cfg(test)]
 mod tests {
+    use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+
     use super::*;
     use crate::decoders;
     use std::path::PathBuf;
@@ -146,5 +152,22 @@ mod tests {
         println!("{result:?}");
         assert!(result.is_ok());
         assert!(fs::remove_file(path).is_ok());
+    }
+
+    #[test]
+    fn test_parallel_encode() {
+        let images = vec![
+            PathBuf::from("images/test1.jpg"),
+            PathBuf::from("images/test2.jpg"),
+            PathBuf::from("images/test3.jpg"),
+            PathBuf::from("images/test4.jpg"),
+        ];
+        let output_format = "png";
+        let quality = 0.75;
+        images.into_par_iter().for_each(|path| {
+            let (pixels, width, height) = decoders::decode_image(&path).unwrap();
+
+            encode_image(&path, &pixels, &output_format, width, height, quality).unwrap();
+        });
     }
 }
