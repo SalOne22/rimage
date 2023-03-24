8c1bb5caa57380e2a4a7a038ae1d0f4688936b44
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-03-24 15:20:13 +0100

    Added: BitDepth, more color spaces, test cases for jpeg, info option, image processing from stdio Changed: rewrite decode jpeg function, rewrite decode png function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use std::{io, path, process};
+
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
-use rimage::{decoders, encoders, Config};
+use rimage::{decoders, encoders, Config, Decoder};
 
 fn main() {
-    let conf = Config::parse_from(wild::args_os());
+    let mut conf = Config::parse_from(wild::args_os());
     let pb = ProgressBar::new(conf.input.len() as u64);
+
+    if conf.input.len() == 0 {
+        conf.input = io::stdin()
+            .lines()
+            .map(|res| {
+                let input_file = res.unwrap();
+                path::PathBuf::from(input_file.trim())
+            })
+            .collect();
+    }
 
     pb.set_style(
         ProgressStyle::with_template("{bar:40.green/blue}  {pos}/{len} {wide_msg}")
@@ -13,11 +25,26 @@ fn main() {
     );
     pb.set_position(0);
 
-    for path in conf.input {
+    if conf.info {
+        for path in &conf.input {
+            let d = Decoder::build(path).unwrap();
+            let img = d.decode().unwrap();
+
+            println!("{:?}", path.file_name().unwrap());
+            println!("Color Space: {:?}", img.color_space());
+            println!("Bit Depth: {:?}", img.bit_depth());
+            println!("Size: {:?}", img.size());
+            println!("Data length: {:?}", img.data_len());
+            println!("");
+        }
+        process::exit(0);
+    }
+
+    for path in &conf.input {
         pb.set_message(path.file_name().unwrap().to_str().unwrap().to_owned());
         pb.inc(1);
 
-        let (pixels, width, height) = match decoders::decode_image(&path) {
+        let (pixels, width, height) = match decoders::decode_image(path) {
             Ok(data) => data,
             Err(e) => {
                 println!("{e}");
@@ -26,7 +53,7 @@ fn main() {
         };
 
         match encoders::encode_image(
-            &path,
+            path,
             &pixels,
             &conf.output_format,
             width,
