4c937ebc739479e305c5c08996ea5400d2e8bde7
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-03-26 12:53:19 +0200

    Rewrite crate structure, add bit depth convert fn

--- src/image.rs
+++ src/image.rs
@@ -19,6 +19,7 @@ use std::{borrow::Cow, fmt};
 /// println!("Data length: {:?}", image.data().len());
 /// # Ok::<(), DecodingError>(())
 /// ```
+#[derive(Debug)]
 pub struct ImageData {
     color_space: ColorSpace,
     bit_depth: BitDepth,
@@ -32,8 +33,8 @@ impl ImageData {
     ///
     /// # Examples
     /// ```
-    /// # use rimage::{ImageData, ColorSpace, BitDepth};
-    /// let image = ImageData::new(ColorSpace::Gray, BitDepth::Eight, 100, 100, vec![0; 100 * 100]);
+    /// # use rimage::{ImageData, image};
+    /// let image = ImageData::new(image::ColorSpace::Gray, image::BitDepth::Eight, 100, 100, vec![0; 100 * 100]);
     /// ```
     pub fn new(
         color_space: ColorSpace,
@@ -69,7 +70,7 @@ impl ImageData {
     ///
     /// # Examples
     /// ```
-    /// # use rimage::{Decoder, ImageData, ColorSpace};
+    /// # use rimage::{Decoder, ImageData, image::ColorSpace};
     /// # use std::path;
     /// # let path = path::PathBuf::from("tests/files/basi0g01.jpg");
     /// # let d = Decoder::build(&path).unwrap();
@@ -108,7 +109,7 @@ impl ImageData {
     ///
     /// # Examples
     /// ```
-    /// # use rimage::{Decoder, ImageData, BitDepth};
+    /// # use rimage::{Decoder, ImageData, image::BitDepth};
     /// # use std::path;
     /// # let path = path::PathBuf::from("tests/files/basi0g01.jpg");
     /// # let d = Decoder::build(&path).unwrap();
@@ -127,6 +128,297 @@ impl ImageData {
     #[inline]
     pub fn bit_depth(&self) -> &BitDepth {
         &self.bit_depth
+    }
+
+    pub fn convert_bit_depth(self, new_bit_depth: BitDepth) -> Self {
+        if self.bit_depth == new_bit_depth {
+            return self;
+        }
+
+        match self.bit_depth {
+            BitDepth::One => match new_bit_depth {
+                BitDepth::One => return self,
+                BitDepth::Two => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 2);
+                    for byte in self.data.iter() {
+                        for i in 0..2 {
+                            let bit = (byte >> i) & 0b11;
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Four => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 4);
+                    for byte in self.data.iter() {
+                        for i in 0..4 {
+                            let bit = (byte >> i) & 0b1111;
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Eight => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 8);
+                    for byte in self.data.iter() {
+                        for i in 0..8 {
+                            let bit = (byte >> i) & 0b1111_1111;
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Sixteen => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 16);
+                    for byte in self.data.iter() {
+                        for i in 0..16 {
+                            let bit = (byte >> i) & 0b1111_1111;
+                            new_data.push(bit);
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+            },
+            BitDepth::Two => match new_bit_depth {
+                BitDepth::One => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 2);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 6) & 0b11;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Two => return self,
+                BitDepth::Four => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 2);
+                    for byte in self.data.iter() {
+                        for i in 0..2 {
+                            let bit = (byte >> i) & 0b11;
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Eight => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 4);
+                    for byte in self.data.iter() {
+                        for i in 0..4 {
+                            let bit = (byte >> i) & 0b11;
+                            new_data.push(bit);
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Sixteen => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 8);
+                    for byte in self.data.iter() {
+                        for i in 0..8 {
+                            let bit = (byte >> i) & 0b11;
+                            new_data.push(bit);
+                            new_data.push(bit);
+                            new_data.push(bit);
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+            },
+            BitDepth::Four => match new_bit_depth {
+                BitDepth::One => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 4);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 4) & 0b1111;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Two => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 2);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 4) & 0b11;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Four => return self,
+                BitDepth::Eight => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 2);
+                    for byte in self.data.iter() {
+                        for i in 0..2 {
+                            let bit = (byte >> i) & 0b1111;
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Sixteen => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 4);
+                    for byte in self.data.iter() {
+                        for i in 0..4 {
+                            let bit = (byte >> i) & 0b1111;
+                            new_data.push(bit);
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+            },
+            BitDepth::Eight => match new_bit_depth {
+                BitDepth::One => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 8);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 8) & 0b1111_1111;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Two => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 4);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 8) & 0b11;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Four => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 2);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 8) & 0b1111;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Eight => return self,
+                BitDepth::Sixteen => {
+                    let mut new_data = Vec::with_capacity(self.data.len() * 2);
+                    for byte in self.data.iter() {
+                        for i in 0..2 {
+                            let bit = (byte >> i) & 0b1111_1111;
+                            new_data.push(bit);
+                            new_data.push(bit);
+                        }
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+            },
+            BitDepth::Sixteen => match new_bit_depth {
+                BitDepth::One => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 16);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 16) & 0b1111_1111;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Two => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 8);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 16) & 0b11;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Four => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 4);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 16) & 0b1111;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Eight => {
+                    let mut new_data = Vec::with_capacity(self.data.len() / 2);
+                    for byte in self.data.iter() {
+                        let bit = (byte >> 16) & 0b1111_1111;
+                        new_data.push(bit);
+                    }
+                    return Self {
+                        data: new_data,
+                        bit_depth: new_bit_depth,
+                        ..self
+                    };
+                }
+                BitDepth::Sixteen => return self,
+            },
+        }
     }
 }
 
@@ -176,7 +468,7 @@ impl fmt::Display for OutputFormat {
 ///
 /// # Examples
 /// ```
-/// # use rimage::ColorSpace;
+/// # use rimage::image::ColorSpace;
 /// # use std::str::FromStr;
 /// let color_space = ColorSpace::from_str("rgb").unwrap();
 /// println!("Color Space: {}", color_space);
@@ -187,7 +479,7 @@ impl fmt::Display for OutputFormat {
 /// - [`ColorSpace::from_str`] if color space is not supported
 ///
 /// [`ColorSpace::from_str`]: enum.ColorSpace.html#method.from_str
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ColorSpace {
     /// **R**ed/**G**reen/**B**lue
     Rgb,
@@ -211,6 +503,43 @@ impl From<png::ColorType> for ColorSpace {
             png::ColorType::Indexed => ColorSpace::Indexed,
             png::ColorType::GrayscaleAlpha => ColorSpace::GrayAlpha,
             png::ColorType::Rgba => ColorSpace::Rgba,
+        }
+    }
+}
+
+impl From<mozjpeg::ColorSpace> for ColorSpace {
+    fn from(color_space: mozjpeg::ColorSpace) -> Self {
+        match color_space {
+            mozjpeg::ColorSpace::JCS_GRAYSCALE => ColorSpace::Gray,
+            mozjpeg::ColorSpace::JCS_CMYK => ColorSpace::Cmyk,
+            mozjpeg::ColorSpace::JCS_RGB => ColorSpace::Rgb,
+            _ => ColorSpace::Rgb,
+        }
+    }
+}
+
+impl Into<mozjpeg::ColorSpace> for ColorSpace {
+    fn into(self) -> mozjpeg::ColorSpace {
+        match self {
+            ColorSpace::Rgb => mozjpeg::ColorSpace::JCS_RGB,
+            ColorSpace::Rgba => mozjpeg::ColorSpace::JCS_RGB,
+            ColorSpace::Cmyk => mozjpeg::ColorSpace::JCS_CMYK,
+            ColorSpace::Indexed => mozjpeg::ColorSpace::JCS_RGB,
+            ColorSpace::Gray => mozjpeg::ColorSpace::JCS_GRAYSCALE,
+            ColorSpace::GrayAlpha => mozjpeg::ColorSpace::JCS_GRAYSCALE,
+        }
+    }
+}
+
+impl Into<png::ColorType> for ColorSpace {
+    fn into(self) -> png::ColorType {
+        match self {
+            ColorSpace::Rgb => png::ColorType::Rgb,
+            ColorSpace::Rgba => png::ColorType::Rgba,
+            ColorSpace::Cmyk => png::ColorType::Rgb,
+            ColorSpace::Indexed => png::ColorType::Indexed,
+            ColorSpace::Gray => png::ColorType::Grayscale,
+            ColorSpace::GrayAlpha => png::ColorType::GrayscaleAlpha,
         }
     }
 }
@@ -248,7 +577,7 @@ impl fmt::Display for ColorSpace {
 ///
 /// # Examples
 /// ```
-/// # use rimage::BitDepth;
+/// # use rimage::image::BitDepth;
 /// # use std::str::FromStr;
 /// let bit_depth = BitDepth::from_str("8").unwrap();
 /// println!("Bit Depth: {}", bit_depth);
@@ -259,7 +588,7 @@ impl fmt::Display for ColorSpace {
 /// - [`BitDepth::from_str`] if bit depth is not supported
 ///
 /// [`BitDepth::from_str`]: enum.BitDepth.html#method.from_str
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum BitDepth {
     /// One bit per pixel
@@ -282,6 +611,18 @@ impl From<png::BitDepth> for BitDepth {
             png::BitDepth::Four => BitDepth::Four,
             png::BitDepth::Eight => BitDepth::Eight,
             png::BitDepth::Sixteen => BitDepth::Sixteen,
+        }
+    }
+}
+
+impl Into<png::BitDepth> for BitDepth {
+    fn into(self) -> png::BitDepth {
+        match self {
+            BitDepth::One => png::BitDepth::One,
+            BitDepth::Two => png::BitDepth::Two,
+            BitDepth::Four => png::BitDepth::Four,
+            BitDepth::Eight => png::BitDepth::Eight,
+            BitDepth::Sixteen => png::BitDepth::Sixteen,
         }
     }
 }
@@ -310,5 +651,31 @@ impl fmt::Display for BitDepth {
             BitDepth::Eight => write!(f, "8"),
             BitDepth::Sixteen => write!(f, "16"),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path;
+
+    use crate::Decoder;
+
+    use super::*;
+
+    #[test]
+    fn test_convert_bit_depth() {
+        let image = Decoder::build(&path::PathBuf::from("tests/files/basi0g01.png"))
+            .unwrap()
+            .decode()
+            .unwrap();
+
+        assert_eq!(image.bit_depth(), &BitDepth::One);
+        assert_eq!(image.data().len(), 128);
+        let image = image.convert_bit_depth(BitDepth::Eight);
+        assert_eq!(image.bit_depth(), &BitDepth::Eight);
+        assert_eq!(image.data().len(), 1024);
+        let image = image.convert_bit_depth(BitDepth::Sixteen);
+        assert_eq!(image.bit_depth(), &BitDepth::Sixteen);
+        assert_eq!(image.data().len(), 4096);
     }
 }
