9d89971cf9f2e9b097f46aae275247f7f34b31c5
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-03-27 14:34:40 +0200

    Rewrite documentation and main function

--- src/image.rs
+++ src/image.rs
@@ -1,27 +1,8 @@
 use std::{borrow::Cow, fmt};
 
-/// Image data from decoder
-///
-/// # Examples
-///
-/// ```
-/// # use rimage::{Decoder, error::DecodingError};
-/// # use std::{path, fs};
-/// # let path = path::PathBuf::from("tests/files/basi0g01.jpg");
-/// let data = fs::read(&path)?;
-/// let d = Decoder::new(&path, &data);
-///
-/// let image = d.decode()?;
-///
-/// // Get something from image data
-/// println!("Color Space: {:?}", image.color_space());
-/// println!("Size: {:?}", image.size());
-/// println!("Data length: {:?}", image.data().len());
-/// # Ok::<(), DecodingError>(())
-/// ```
+/// Image data
 #[derive(Debug, Clone)]
 pub struct ImageData {
-    color_space: ColorSpace,
     width: usize,
     height: usize,
     data: Vec<u8>,
@@ -33,88 +14,28 @@ impl ImageData {
     /// # Examples
     /// ```
     /// # use rimage::{ImageData, image};
-    /// let image = ImageData::new(image::ColorSpace::Gray, 100, 100, vec![0; 100 * 100]);
+    /// let image = ImageData::new(100, 100, vec![0; 100 * 100]);
     /// ```
-    pub fn new(color_space: ColorSpace, width: usize, height: usize, data: Vec<u8>) -> Self {
+    pub fn new(width: usize, height: usize, data: Vec<u8>) -> Self {
         Self {
-            color_space,
             width,
             height,
             data,
         }
     }
-    /// Returns size of image (Width, Height)
-    ///
-    /// # Examples
-    /// ```
-    /// # use rimage::{Decoder, ImageData};
-    /// # use std::path;
-    /// # let path = path::Path::new("tests/files/basi0g01.jpg");
-    /// # let data = std::fs::read(&path).unwrap();
-    /// # let d = Decoder::new(&path, &data);
-    /// # let image = d.decode().unwrap();
-    /// let (width, height) = image.size();
-    /// ```
+    /// Get the width and height of the image
     #[inline]
     pub fn size(&self) -> (usize, usize) {
         (self.width, self.height)
     }
-    /// Returns a ref to color space of image [`ColorSpace`]
-    ///
-    /// # Examples
-    /// ```
-    /// # use rimage::{Decoder, ImageData, image::ColorSpace};
-    /// # use std::path;
-    /// # let path = path::Path::new("tests/files/basi0g01.jpg");
-    /// # let data = std::fs::read(&path).unwrap();
-    /// # let d = Decoder::new(&path, &data);
-    /// # let image = d.decode().unwrap();
-    /// let color_space = image.color_space();
-    /// match color_space {
-    ///     ColorSpace::Gray => println!("Grayscale"),
-    ///     ColorSpace::Rgb => println!("RGB"),
-    ///     ColorSpace::Cmyk => println!("CMYK"),
-    ///     ColorSpace::Rgba => println!("RGBA"),
-    ///     ColorSpace::Indexed => println!("Indexed"),
-    ///     ColorSpace::GrayAlpha => println!("Grayscale Alpha"),
-    /// }
-    /// ```
-    /// [`ColorSpace`]: enum.ColorSpace.html
-    #[inline]
-    pub fn color_space(&self) -> &ColorSpace {
-        &self.color_space
-    }
-    /// Returns a ref to array of bytes in image
-    ///
-    /// # Examples
-    /// ```
-    /// # use rimage::{Decoder, ImageData};
-    /// # use std::path;
-    /// # let path = path::Path::new("tests/files/basi0g01.jpg");
-    /// # let data = std::fs::read(&path).unwrap();
-    /// # let d = Decoder::new(&path, &data);
-    /// # let image = d.decode().unwrap();
-    /// let data = image.data();
-    /// ```
+    /// Get image data
     #[inline]
     pub fn data(&self) -> &[u8] {
         &self.data
     }
-    #[inline]
-    pub(crate) fn data_mut(&mut self) -> &mut Vec<u8> {
-        &mut self.data
-    }
 }
 
-/// Supported output format
-///
-/// # Examples
-/// ```
-/// # use rimage::OutputFormat;
-/// # use std::str::FromStr;
-/// let format = OutputFormat::from_str("mozjpeg").unwrap();
-/// println!("Format: {}", format);
-/// ```
+/// Image format to encode
 #[derive(Debug, Clone, Copy)]
 pub enum OutputFormat {
     /// MozJpeg image
@@ -144,115 +65,6 @@ impl fmt::Display for OutputFormat {
             OutputFormat::MozJpeg => write!(f, "jpeg"),
             OutputFormat::Png => write!(f, "png"),
             OutputFormat::Oxipng => write!(f, "oxipng"),
-        }
-    }
-}
-
-/// Color space of image
-///
-/// # Examples
-/// ```
-/// # use rimage::image::ColorSpace;
-/// # use std::str::FromStr;
-/// let color_space = ColorSpace::from_str("rgb").unwrap();
-/// println!("Color Space: {}", color_space);
-/// ```
-///
-/// # Errors
-///
-/// - [`ColorSpace::from_str`] if color space is not supported
-///
-/// [`ColorSpace::from_str`]: enum.ColorSpace.html#method.from_str
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ColorSpace {
-    /// **R**ed/**G**reen/**B**lue
-    Rgb,
-    /// **R**ed/**G**reen/**B**lue/**A**lpha
-    Rgba,
-    /// **C**yan/**M**agenta/**Y**ellow/Blac**K**
-    Cmyk,
-    /// Indexed color palette
-    Indexed,
-    /// Grayscale
-    Gray,
-    /// Grayscale/Alpha
-    GrayAlpha,
-}
-
-impl From<png::ColorType> for ColorSpace {
-    fn from(color_type: png::ColorType) -> Self {
-        match color_type {
-            png::ColorType::Grayscale => ColorSpace::Gray,
-            png::ColorType::Rgb => ColorSpace::Rgb,
-            png::ColorType::Indexed => ColorSpace::Indexed,
-            png::ColorType::GrayscaleAlpha => ColorSpace::GrayAlpha,
-            png::ColorType::Rgba => ColorSpace::Rgba,
-        }
-    }
-}
-
-impl From<mozjpeg::ColorSpace> for ColorSpace {
-    fn from(color_space: mozjpeg::ColorSpace) -> Self {
-        match color_space {
-            mozjpeg::ColorSpace::JCS_GRAYSCALE => ColorSpace::Gray,
-            mozjpeg::ColorSpace::JCS_CMYK => ColorSpace::Cmyk,
-            mozjpeg::ColorSpace::JCS_RGB => ColorSpace::Rgb,
-            _ => ColorSpace::Rgb,
-        }
-    }
-}
-
-impl Into<mozjpeg::ColorSpace> for ColorSpace {
-    fn into(self) -> mozjpeg::ColorSpace {
-        match self {
-            ColorSpace::Rgb => mozjpeg::ColorSpace::JCS_RGB,
-            ColorSpace::Rgba => mozjpeg::ColorSpace::JCS_EXT_RGBA,
-            ColorSpace::Cmyk => mozjpeg::ColorSpace::JCS_CMYK,
-            ColorSpace::Indexed => mozjpeg::ColorSpace::JCS_RGB,
-            ColorSpace::Gray => mozjpeg::ColorSpace::JCS_GRAYSCALE,
-            ColorSpace::GrayAlpha => mozjpeg::ColorSpace::JCS_GRAYSCALE,
-        }
-    }
-}
-
-impl Into<png::ColorType> for ColorSpace {
-    fn into(self) -> png::ColorType {
-        match self {
-            ColorSpace::Rgb => png::ColorType::Rgb,
-            ColorSpace::Rgba => png::ColorType::Rgba,
-            ColorSpace::Cmyk => png::ColorType::Rgb,
-            ColorSpace::Indexed => png::ColorType::Indexed,
-            ColorSpace::Gray => png::ColorType::Grayscale,
-            ColorSpace::GrayAlpha => png::ColorType::GrayscaleAlpha,
-        }
-    }
-}
-
-impl std::str::FromStr for ColorSpace {
-    type Err = Cow<'static, str>;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "rgb" => Ok(ColorSpace::Rgb),
-            "rgba" => Ok(ColorSpace::Rgba),
-            "cmyk" => Ok(ColorSpace::Cmyk),
-            "indexed" => Ok(ColorSpace::Indexed),
-            "grayscale" => Ok(ColorSpace::Gray),
-            "grayscale_alpha" => Ok(ColorSpace::GrayAlpha),
-            _ => Err(format!("{} is not a valid color space", s).into()),
-        }
-    }
-}
-
-impl fmt::Display for ColorSpace {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ColorSpace::Rgb => write!(f, "rgb"),
-            ColorSpace::Rgba => write!(f, "rgba"),
-            ColorSpace::Cmyk => write!(f, "cmyk"),
-            ColorSpace::Indexed => write!(f, "indexed"),
-            ColorSpace::Gray => write!(f, "grayscale"),
-            ColorSpace::GrayAlpha => write!(f, "grayscale_alpha"),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::{fs, io, path, process};
+use std::{error::Error, fs, io, path, process};
 
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
-use rimage::{decoders, encoders, image::OutputFormat, Decoder};
+use rimage::{decoders, encoders, image::OutputFormat, Config, Decoder, Encoder};
 
 #[derive(Parser)]
 #[command(author, about, version, long_about = None)]
@@ -20,7 +20,7 @@ struct Args {
     info: bool,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut args = Args::parse_from(wild::args_os());
     let pb = ProgressBar::new(args.input.len() as u64);
 
@@ -34,16 +34,18 @@ fn main() {
             .collect();
     }
 
+    let conf = Config::build(args.quality, args.output_format)?;
+
     pb.set_style(
         ProgressStyle::with_template("{bar:40.green/blue}  {pos}/{len} {wide_msg}")
             .unwrap()
-            .progress_chars("##*"),
+            .progress_chars("##-"),
     );
     pb.set_position(0);
 
     if args.info {
         for path in &args.input {
-            let data = fs::read(path).unwrap();
+            let data = fs::read(path)?;
             let d = Decoder::new(path, &data);
 
             let img = match d.decode() {
@@ -55,7 +57,6 @@ fn main() {
             };
 
             println!("{:?}", path.file_name().unwrap());
-            println!("Color Space: {:?}", img.color_space());
             println!("Size: {:?}", img.size());
             println!("Data length: {:?}", img.data().len());
             println!();
@@ -67,28 +68,12 @@ fn main() {
         pb.set_message(path.file_name().unwrap().to_str().unwrap().to_owned());
         pb.inc(1);
 
-        let (pixels, width, height) = match decoders::decode_image(path) {
-            Ok(data) => data,
-            Err(e) => {
-                println!("{e}");
-                continue;
-            }
-        };
+        let data = fs::read(&path)?;
 
-        match encoders::encode_image(
-            path,
-            &pixels,
-            &args.output_format.to_string(),
-            width,
-            height,
-            args.quality,
-        ) {
-            Ok(()) => (),
-            Err(e) => {
-                println!("{e}");
-                continue;
-            }
-        };
+        let d = Decoder::new(&path, &data);
+        let e = Encoder::new(&conf, d.decode()?);
+        fs::write(path, e.encode()?)?
     }
     pb.finish();
+    Ok(())
 }
