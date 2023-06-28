d9a45cdc435471fdfc36c5361595592478645ade
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-06-28 12:42:16 +0200

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ Similar to [squoosh!](https://squoosh.app/) using same codecs,
 but fully written on rust and with bulk processing support.
 
 Current features:
-- Decoding jpeg and png
+- Decoding jpeg, png, webp and avif
 - Encoding with optimizations
-- Get image information
+- Getting image information
 
 # Usage
 
@@ -23,6 +23,71 @@ rimage = "0.7"
 
 After that you can use this crate:
 
+## Easy way
+```
+use rimage::{image::OutputFormat, optimize, Config};
+
+// Get file path
+let path = std::path::PathBuf::from("tests/files/basi0g01.jpg"); // Or any other image
+
+// Build config for encoding
+let config = match Config::build(75.0, OutputFormat::MozJpeg) {
+    Ok(config) => config,
+    Err(e) => {
+        eprintln!("Oh no, there is error! {e}");
+        std::process::exit(1);
+    }
+};
+
+// Get encoded image data from encoder
+let data = match optimize(&path, &config) {
+    Ok(data) => data,
+    Err(e) => {
+        eprintln!("Oh no, there is error! {e}");
+        std::process::exit(1);
+    }
+};
+
+// Write image to file
+std::fs::write("output.jpg", data);
+# std::fs::remove_file("output.jpg").unwrap();
+```
+
+### Get optimized image from memory
+```
+use std::{io::Read, path, fs};
+use rimage::{image::{OutputFormat, InputFormat}, optimize_from_memory, Config};
+
+// Get file data from path
+let path = path::PathBuf::from("tests/files/basi0g01.jpg"); // Or any other image
+let mut file = fs::File::open(path).unwrap();
+let metadata = file.metadata().unwrap();
+let mut data = Vec::with_capacity(metadata.len() as usize);
+file.read_to_end(&mut data).unwrap();
+
+// Build config for encoding
+let config = match Config::build(75.0, OutputFormat::MozJpeg) {
+    Ok(config) => config,
+    Err(e) => {
+        eprintln!("Oh no, there is error! {e}");
+        std::process::exit(1);
+    }
+};
+
+// Get encoded image data from encoder
+let data = match optimize_from_memory(&data, InputFormat::Jpeg, &config) {
+    Ok(data) => data,
+    Err(e) => {
+        eprintln!("Oh no, there is error! {e}");
+        std::process::exit(1);
+    }
+};
+
+// Write image to file
+fs::write("output.jpg", data);
+# fs::remove_file("output.jpg").unwrap();
+```
+
 ## Decoding
 ```
 use rimage::Decoder;
@@ -31,6 +96,37 @@ use rimage::Decoder;
 let path = std::path::PathBuf::from("tests/files/basi0g01.jpg"); // Or any other image
 let file = std::fs::File::open(&path).unwrap();
 let decoder = Decoder::new(&path, file);
+
+// Decode image to image data
+let image = match decoder.decode() {
+    Ok(img) => img,
+    Err(e) => {
+        eprintln!("Oh no, there is error! {e}");
+        std::process::exit(1);
+    }
+};
+
+// Get image data
+println!("Size: {:?}", image.size());
+println!("Data length: {:?}", image.data().len());
+
+// Do something with image...
+```
+
+### Decoding from memory
+```
+use std::{io::Read, path, fs};
+use rimage::{MemoryDecoder, image::InputFormat};
+
+// Get file data
+let path = path::PathBuf::from("tests/files/basi0g01.jpg"); // Or any other image
+let mut file = fs::File::open(path).unwrap();
+let metadata = file.metadata().unwrap();
+let mut data = Vec::with_capacity(metadata.len() as usize);
+file.read_to_end(&mut data).unwrap();
+
+// Create decoder from file data and input format
+let decoder = MemoryDecoder::new(&data, InputFormat::Jpeg);
 
 // Decode image to image data
 let image = match decoder.decode() {
@@ -59,13 +155,7 @@ use rimage::{Config, Encoder, OutputFormat};
 # let image = decoder.decode().unwrap();
 
 // Build config for encoding
-let config = match Config::build(
-    75.0,
-    OutputFormat::MozJpeg,
-    None,
-    None,
-    None,
-) {
+let config = match Config::build(75.0, OutputFormat::MozJpeg) {
     Ok(config) => config,
     Err(e) => {
         eprintln!("Oh no, there is error! {e}");
