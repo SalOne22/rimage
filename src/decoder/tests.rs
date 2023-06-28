7b65d6fb716e07717452df40296f0bd68eed4c1e
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-06-28 11:37:03 +0200

    Organized tests

--- src/decoder/tests.rs
+++ src/decoder/tests.rs
@@ -1,8 +1,21 @@
 use std::fs;
 
-use regex::Regex;
+use crate::test_utils::get_files_by_regex;
 
 use super::*;
+
+fn decode_files<F>(files: &[path::PathBuf], callback: F)
+where
+    F: Fn(Result<ImageData, DecodingError>),
+{
+    files.iter().for_each(|path| {
+        println!("{path:?}");
+        let file = fs::File::open(path).unwrap();
+        let image = Decoder::new(path, file).decode();
+
+        callback(image)
+    });
+}
 
 #[test]
 fn decode_unsupported() {
@@ -17,248 +30,115 @@ fn decode_unsupported() {
 
 #[test]
 fn decode_grayscale() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^tests/files/[^x]&[^t].+0g\d\d((\.png)|(\.jpg))").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/[^x]&[^t].+0g\d\d((\.png)|(\.jpg))");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let image = Decoder::new(path, file).decode().unwrap();
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
         assert_ne!(image.data().len(), 0);
         assert_ne!(image.size(), (0, 0));
-    })
+    });
 }
 
 #[test]
 fn decode_grayscale_alpha() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^tests/files/[^x].+4a\d\d\.png").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/[^x].+4a\d\d\.png");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let image = Decoder::new(path, file).decode().unwrap();
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
         assert_ne!(image.data().len(), 0);
         assert_ne!(image.size(), (0, 0));
-    })
+    });
 }
 
 #[test]
 fn decode_rgb() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^^tests/files/[^x]&[^t].+2c\d\d((\.png)|(\.jpg))").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/[^x]&[^t].+2c\d\d((\.png)|(\.jpg))");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let image = Decoder::new(path, file).decode().unwrap();
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
         assert_ne!(image.data().len(), 0);
         assert_ne!(image.size(), (0, 0));
-    })
+    });
 }
 
 #[test]
 fn decode_rgb_transparent() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^^tests/files/[^x]&[t].+2c\d\d((\.png)|(\.jpg))").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/[^x]&[t].+2c\d\d((\.png)|(\.jpg))");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let image = Decoder::new(path, file).decode().unwrap();
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
         assert_ne!(image.data().len(), 0);
         assert_ne!(image.size(), (0, 0));
-    })
+    });
 }
 
 #[test]
 fn decode_rgba() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^tests/files/[^x].+6a\d\d\.png$").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/[^x].+6a\d\d\.png$");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let image = Decoder::new(path, file).decode().unwrap();
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
         assert_ne!(image.data().len(), 0);
         assert_ne!(image.size(), (0, 0));
-    })
+    });
 }
 
 #[test]
 fn decode_indexed() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^tests/files/[^x]&[^t].+3p\d\d\.png$").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/[^x]&[^t].+3p\d\d\.png$");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let image = Decoder::new(path, file).decode().unwrap();
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
         assert_ne!(image.data().len(), 0);
         assert_ne!(image.size(), (0, 0));
-    })
+    });
 }
 
 #[test]
 fn decode_indexed_transparent() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^tests/files/[^x]&[t].+3p\d\d\.png$").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/[^x]&[t].+3p\d\d\.png$");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let image = Decoder::new(path, file).decode().unwrap();
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
         assert_ne!(image.data().len(), 0);
         assert_ne!(image.size(), (0, 0));
-    })
+    });
 }
 
 #[test]
 fn decode_corrupted() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^tests/files/x.+\d\d\.png$").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/x.+\d\d\.png$");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let d = Decoder::new(path, file);
-
-        let img = d.decode();
-        assert!(img.is_err());
-    })
+    decode_files(&files, |image| assert!(image.is_err()));
 }
 
 #[test]
 fn decode_webp() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^tests/files/.+.webp$").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/.+.webp$");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let d = Decoder::new(path, file);
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
-        let img = d.decode().unwrap();
-        println!("{:?}", img.size());
-        println!("{:?}", img.data().len());
-
-        assert_ne!(img.data().len(), 0);
-        assert_ne!(img.size(), (0, 0));
-    })
+        assert_ne!(image.data().len(), 0);
+        assert_ne!(image.size(), (0, 0));
+    });
 }
 
 #[test]
 fn decode_avif() {
-    let files: Vec<path::PathBuf> = fs::read_dir("tests/files/")
-        .unwrap()
-        .map(|entry| {
-            let entry = entry.unwrap();
-            entry.path()
-        })
-        .filter(|path| {
-            let re = Regex::new(r"^tests/files/.+.avif$").unwrap();
-            re.is_match(path.to_str().unwrap_or(""))
-        })
-        .collect();
+    let files = get_files_by_regex(r"^tests/files/.+.avif$");
 
-    files.iter().for_each(|path| {
-        println!("{path:?}");
-        let file = fs::File::open(path).unwrap();
-        let d = Decoder::new(path, file);
+    decode_files(&files, |image| {
+        let image = image.unwrap();
 
-        let img = d.decode().unwrap();
-        println!("{:?}", img.size());
-        println!("{:?}", img.data().len());
-
-        assert_ne!(img.data().len(), 0);
-        assert_ne!(img.size(), (0, 0));
-    })
+        assert_ne!(image.data().len(), 0);
+        assert_ne!(image.size(), (0, 0));
+    });
 }
