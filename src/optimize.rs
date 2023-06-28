4b3746ef951bff2bab3162b3c1acb59e7fa5bc79
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-06-28 12:52:57 +0200

    Added docs to optimize functions

--- src/optimize.rs
+++ src/optimize.rs
@@ -5,6 +5,25 @@ use log::info;
 use crate::{image::InputFormat, Config, Decoder, Encoder, MemoryDecoder};
 
 /// Optimizes one image with provided config
+///
+/// # Example
+/// ```
+/// # use rimage::{Config, optimize, image::OutputFormat};
+/// // Get file path
+/// let path = std::path::PathBuf::from("tests/files/basi0g01.jpg");
+///
+/// // Build config for encoding
+/// let config = Config::build(75.0, OutputFormat::MozJpeg).unwrap();
+///
+/// // Get encoded image data from encoder
+/// let data = optimize(&path, &config).unwrap();
+///
+/// // Do something with image data...
+/// ```
+///
+/// # Errors
+///
+/// This function can return any error that happens during decoding and encoding process
 pub fn optimize(image_path: &path::Path, config: &Config) -> Result<Vec<u8>, Box<dyn Error>> {
     let file = fs::File::open(image_path)?;
 
@@ -17,6 +36,31 @@ pub fn optimize(image_path: &path::Path, config: &Config) -> Result<Vec<u8>, Box
 }
 
 /// Optimizes one image from memory with provided config
+///
+/// # Example
+/// ```
+/// use std::io::Read;
+/// # use rimage::{optimize_from_memory, image::{InputFormat, OutputFormat}, Config};
+///
+/// // Get file data
+/// let path = std::path::PathBuf::from("tests/files/basi0g01.jpg");
+/// let mut file = std::fs::File::open(path).unwrap();
+/// let metadata = file.metadata().unwrap();
+/// let mut data = Vec::with_capacity(metadata.len() as usize);
+/// file.read_to_end(&mut data).unwrap();
+///
+/// // Build config for encoding
+/// let config = Config::build(75.0, OutputFormat::MozJpeg).unwrap();
+///
+/// // Get encoded image data from encoder
+/// let data = optimize_from_memory(&data, InputFormat::Jpeg, &config).unwrap();
+///
+/// // Do something with image data...
+/// ```
+///
+/// # Errors
+///
+/// This function can return any error that happens during decoding and encoding process
 pub fn optimize_from_memory(
     data: &[u8],
     input_format: InputFormat,
