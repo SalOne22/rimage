ed747bb59f5c73ac3ec2c290ec272abfdc33897a
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-06-29 15:34:14 +0200

    Added more tests to config

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -1,18 +1,69 @@
 use super::*;
 
 #[test]
-fn config_edge_cases() {
-    let config = Config::default();
-    assert_eq!(config.output_format, OutputFormat::MozJpeg);
-    assert_eq!(config.quality, 75.0);
-    let config = Config::build(100.0, OutputFormat::Png).unwrap();
-    assert_eq!(config.output_format, OutputFormat::Png);
-    assert_eq!(config.quality, 100.0);
-    let config = Config::build(0.0, OutputFormat::Oxipng).unwrap();
-    assert_eq!(config.output_format, OutputFormat::Oxipng);
-    assert_eq!(config.quality, 0.0);
-    let config_result = Config::build(101.0, OutputFormat::MozJpeg);
-    assert!(config_result.is_err());
-    let config_result = Config::build(-1.0, OutputFormat::MozJpeg);
-    assert!(config_result.is_err());
+fn build_edge_cases() {
+    let conf = Config::build(100.0, OutputFormat::MozJpeg);
+    assert!(conf.is_ok());
+
+    let conf = Config::build(50.0, OutputFormat::MozJpeg);
+    assert!(conf.is_ok());
+
+    let conf = Config::build(0.0, OutputFormat::MozJpeg);
+    assert!(conf.is_ok());
+
+    let conf = Config::build(-10.0, OutputFormat::MozJpeg);
+    assert!(conf.is_err());
+
+    let conf = Config::build(110.0, OutputFormat::MozJpeg);
+    assert!(conf.is_err());
+}
+
+#[test]
+fn quality_edge_cases() {
+    let mut conf = Config::build(100.0, OutputFormat::MozJpeg).unwrap();
+
+    assert!(conf.set_quality(0.0).is_ok());
+    assert!(conf.set_quality(50.0).is_ok());
+    assert!(conf.set_quality(100.0).is_ok());
+    assert!(conf.set_quality(-10.0).is_err());
+    assert!(conf.set_quality(110.0).is_err());
+}
+
+#[test]
+fn target_width_edge_cases() {
+    let mut conf = Config::build(100.0, OutputFormat::MozJpeg).unwrap();
+
+    assert!(conf.set_target_width(Some(0)).is_err());
+    assert!(conf.set_target_width(Some(50)).is_ok());
+    assert!(conf.set_target_width(Some(usize::MAX)).is_ok());
+}
+
+#[test]
+fn target_height_edge_cases() {
+    let mut conf = Config::build(100.0, OutputFormat::MozJpeg).unwrap();
+
+    assert!(conf.set_target_height(Some(0)).is_err());
+    assert!(conf.set_target_height(Some(50)).is_ok());
+    assert!(conf.set_target_height(Some(usize::MAX)).is_ok());
+}
+
+#[test]
+fn quantization_quality_edge_cases() {
+    let mut conf = Config::build(100.0, OutputFormat::MozJpeg).unwrap();
+
+    assert!(conf.set_quantization_quality(Some(0)).is_ok());
+    assert!(conf.set_quantization_quality(Some(50)).is_ok());
+    assert!(conf.set_quantization_quality(Some(100)).is_ok());
+    assert!(conf.set_quantization_quality(Some(110)).is_err());
+}
+
+#[test]
+fn dithering_level_edge_cases() {
+    let mut conf = Config::build(100.0, OutputFormat::MozJpeg).unwrap();
+
+    assert!(conf.set_dithering_level(Some(0.0)).is_ok());
+    assert!(conf.set_dithering_level(Some(0.5)).is_ok());
+    assert!(conf.set_dithering_level(Some(1.0)).is_ok());
+    assert!(conf.set_dithering_level(Some(-1.0)).is_err());
+    assert!(conf.set_dithering_level(Some(1.1)).is_err());
 }
