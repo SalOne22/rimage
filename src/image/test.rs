24da7852e2e1452263fbb7afedaca2fca2f6e89e
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-09-07 00:20:48 +0200

    Added image module

---
+++ src/image/test.rs
@@ -0,0 +1,63 @@
+use super::*;
+
+#[test]
+fn new_image() {
+    let pixel_data: Vec<RGBA8> = vec![RGBA8::new(0, 0, 0, 0); 100 * 50];
+    let image = Image::new(pixel_data.clone(), 100, 50);
+
+    assert_eq!(image.data(), &pixel_data);
+    assert_eq!(image.width(), 100);
+    assert_eq!(image.height(), 50);
+}
+
+#[test]
+fn resize_image_smaller() {
+    let image_data: Vec<RGBA8> = vec![RGBA8::new(0, 0, 0, 0); 100 * 50];
+    let mut image = Image::new(image_data, 100, 50);
+
+    let resize_config = ResizeConfig::default().with_width(50);
+    image.resize(&resize_config).unwrap();
+
+    assert_eq!(image.data(), &[RGBA8::new(0, 0, 0, 0); 50 * 25]);
+    assert_eq!(image.width(), 50);
+    assert_eq!(image.height(), 25);
+}
+
+#[test]
+fn resize_image_bigger() {
+    let image_data: Vec<RGBA8> = vec![RGBA8::new(0, 0, 0, 0); 100 * 50];
+    let mut image = Image::new(image_data, 100, 50);
+
+    let resize_config = ResizeConfig::default().with_width(200);
+    image.resize(&resize_config).unwrap();
+
+    assert_eq!(image.data(), &vec![RGBA8::new(0, 0, 0, 0); 200 * 100]);
+    assert_eq!(image.width(), 200);
+    assert_eq!(image.height(), 100);
+}
+
+#[test]
+fn resize_image_width_and_height() {
+    let image_data: Vec<RGBA8> = vec![RGBA8::new(0, 0, 0, 0); 100 * 50];
+    let mut image = Image::new(image_data, 100, 50);
+
+    let resize_config = ResizeConfig::default().with_width(200).with_height(150);
+    image.resize(&resize_config).unwrap();
+
+    assert_eq!(image.data(), &[RGBA8::new(0, 0, 0, 0); 200 * 150]);
+    assert_eq!(image.width(), 200);
+    assert_eq!(image.height(), 150);
+}
+
+#[test]
+fn quantize_image() {
+    let image_data: Vec<RGBA8> = vec![RGBA8::new(0, 0, 0, 0); 800 * 600];
+    let mut image = Image::new(image_data.clone(), 800, 600);
+
+    let quantization_config = QuantizationConfig::default();
+    assert!(image.quantize(&quantization_config).is_ok());
+
+    // Test quantization with quality
+    let quantization_config = QuantizationConfig::default().with_quality(50).unwrap();
+    assert!(image.quantize(&quantization_config).is_ok());
+}
