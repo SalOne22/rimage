648bc646febb8b135da06b2af753f5f56b470ef0
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-09-07 00:20:16 +0200

    Added config modules

---
+++ src/config/codec.rs
@@ -0,0 +1,34 @@
+/// Enum representing various image codecs supported by rimage.
+///
+/// This enum defines the codecs that can be used for encoding and decoding images.
+/// Each variant corresponds to a specific image codec, allowing you to specify the
+/// desired codec when working with images.
+///
+/// # Examples
+///
+/// ```
+/// use rimage::config::Codec;
+///
+/// let jpeg_codec = Codec::MozJpeg;
+/// let png_codec = Codec::Png;
+///
+/// // Use the codec with image processing functions.
+/// // ...
+/// ```
+#[derive(Debug, PartialEq, Eq)]
+pub enum Codec {
+    /// Mozilla JPEG codec for image encoding and decoding.
+    MozJpeg,
+
+    /// PNG codec for image encoding and decoding.
+    Png,
+
+    /// OxiPNG codec for better PNG image optimization.
+    OxiPng,
+
+    /// WebP codec for image encoding and decoding with WebP format.
+    WebP,
+
+    /// AVIF codec for image encoding and decoding.
+    Avif,
+}
---
+++ src/config/mod.rs
@@ -0,0 +1,9 @@
+mod codec;
+mod encoder_config;
+mod quantization_config;
+mod resize_config;
+
+pub use codec::Codec;
+pub use encoder_config::EncoderConfig;
+pub use quantization_config::QuantizationConfig;
+pub use resize_config::ResizeConfig;
