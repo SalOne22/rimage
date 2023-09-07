32364ca85e51fc137fa9a82990123b5e778c598d
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-09-07 15:03:59 +0200

    Updated documentation

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,58 @@ Rimage is a powerful Rust image optimization library designed to simplify and en
 4. **Quantization and Dithering**: For advanced users, Rimage offers control over quantization and dithering.
 5. **Image Resizing**: Resize images with ease using `resize` crate.
 
-## Encoding
+## Decoding
 
-Basic usage:
+From path:
 
 ```
-use rimage::config::{EncoderConfig, Codec};
+use rimage::Decoder;
+
+let path = std::path::PathBuf::from("tests/files/basi0g01.jpg");
+
+let decoder = Decoder::from_path(&path)?;
+
+let image = decoder.decode()?;
+
+// do something with the image data...
+# Ok::<(), rimage::error::DecoderError>(())
+```
+
+From memory:
+```
+# use std::fs::File;
+use rimage::{Decoder, config::ImageFormat};
+
+# let f = File::open("tests/files/basi0g01.jpg").unwrap();
+let reader = std::io::BufReader::new(f); // you can use any reader
+
+let decoder = Decoder::new(reader).with_format(ImageFormat::Jpeg);
+
+let image = decoder.decode()?;
+
+// do something with the image data...
+# Ok::<(), rimage::error::DecoderError>(())
+```
+
+## Encoding
+
+```
+use std::fs::File;
+
+use rimage::{rgb::RGBA8, Encoder, Image, config::{EncoderConfig, Codec}};
+
+let image_data = vec![RGBA8::new(0, 0, 0, 0); 100 * 50];
+let image = Image::new(image_data, 100, 50);
 
 let config = EncoderConfig::new(Codec::MozJpeg).with_quality(80.0).unwrap();
+let file = File::create("output.jpg").expect("Failed to create file");
+
+let encoder = Encoder::new(file, image).with_config(config);
+
+encoder.encode()?;
+
+# std::fs::remove_file("output.jpg").unwrap();
+# Ok::<(), rimage::error::EncoderError>(())
 ```
 */
 
