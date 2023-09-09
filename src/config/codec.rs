891b1f1599530c6320292317a749f3766a46d2e4
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-09-09 13:45:07 +0200

    Updated codec enum

--- src/config/codec.rs
+++ src/config/codec.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 /// Enum representing various image codecs supported by rimage.
 ///
 /// This enum defines the codecs that can be used for encoding and decoding images.
@@ -15,7 +17,7 @@
 /// // Use the codec with image processing functions.
 /// // ...
 /// ```
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Codec {
     /// Mozilla JPEG codec for image encoding and decoding.
     MozJpeg,
@@ -38,4 +40,25 @@ pub enum Codec {
     /// AVIF codec for image encoding and decoding.
     #[cfg(feature = "avif")]
     Avif,
+}
+
+impl FromStr for Codec {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.to_lowercase().as_str() {
+            "mozjpeg" | "jpeg" | "jpg" => Self::MozJpeg,
+            "png" => Self::Png,
+            #[cfg(feature = "jxl")]
+            "jpegxl" | "jxl" => Self::JpegXl,
+            #[cfg(feature = "oxipng")]
+            "oxipng" => Self::OxiPng,
+            #[cfg(feature = "webp")]
+            "webp" => Self::WebP,
+            #[cfg(feature = "avif")]
+            "avif" => Self::Avif,
+
+            codec => return Err(format!("{codec} is not supported codec.")),
+        })
+    }
 }
