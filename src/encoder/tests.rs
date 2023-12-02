9f3cb75606ea9ced7791fc88482c2cde7682ae27
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-12-03 00:16:16 +0100

    Added integration with image crate ! With Bugs

--- src/encoder/tests.rs
+++ src/encoder/tests.rs
@@ -1,6 +1,5 @@
+use image::RgbaImage;
 use std::io::Cursor;
-
-use rgb::RGBA8;
 
 // Import the necessary dependencies from the code
 use super::*;
@@ -9,35 +8,35 @@ use crate::config::Codec;
 #[test]
 fn encoder_new() {
     // Create a mock image and writer
-    let image_data = vec![RGBA8::new(0, 0, 0, 0); 100 * 50];
-    let image = Image::new(image_data.clone(), 100, 50);
+    let image_data = vec![0; 100 * 50 * 4];
+    let image = RgbaImage::from_raw(100, 50, image_data).unwrap();
     let writer = Cursor::new(Vec::new());
 
     // Create an Encoder with default config
-    let encoder = Encoder::new(writer.clone(), image);
+    let encoder = Encoder::new(writer.clone(), DynamicImage::ImageRgba8(image));
 
     // Verify that the Encoder was created with the correct properties
     assert_eq!(encoder.w, writer);
-    assert_eq!(encoder.data.data(), &[RGBA8::new(0, 0, 0, 0); 100 * 50]);
+    assert_eq!(encoder.data.as_bytes(), &[0; 100 * 50 * 4]);
     assert_eq!(encoder.conf.codec(), &Codec::MozJpeg);
 }
 
 #[test]
 fn encoder_with_config() {
     // Create a mock image and writer
-    let image_data = vec![RGBA8::new(0, 0, 0, 0); 100 * 50];
-    let image = Image::new(image_data.clone(), 100, 50);
+    let image_data = vec![0; 100 * 50 * 4];
+    let image = RgbaImage::from_raw(100, 50, image_data).unwrap();
     let writer = Cursor::new(Vec::new());
 
     // Create an Encoder with a custom config
     let config = EncoderConfig::new(Codec::MozJpeg)
         .with_quality(90.0)
         .unwrap();
-    let encoder = Encoder::new(writer.clone(), image).with_config(config);
+    let encoder = Encoder::new(writer.clone(), DynamicImage::ImageRgba8(image)).with_config(config);
 
     // Verify that the Encoder was created with the correct properties
     assert_eq!(encoder.w, writer);
-    assert_eq!(encoder.data.data(), &[RGBA8::new(0, 0, 0, 0); 100 * 50]);
+    assert_eq!(encoder.data.as_bytes(), &[0; 100 * 50 * 4]);
     assert_eq!(encoder.conf.codec(), &Codec::MozJpeg);
     assert_eq!(encoder.conf.quality(), 90.0);
 }
