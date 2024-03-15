8a19d4b71ed7e586b1b7a119ad83ff7369cd4bc3
Vladinov Vladyslav <vladinov.dev@gmail.com>
2024-03-16 00:04:49 +0100

    feat: added long_about to jxl codec

--- src/cli/codecs/jpeg_xl.rs
+++ src/cli/codecs/jpeg_xl.rs
@@ -1,7 +1,11 @@
 use clap::Command;
+use indoc::indoc;
 
 pub fn jpeg_xl() -> Command {
     Command::new("jpeg_xl")
-        .about("Encode images into jpeg xl format")
         .alias("jxl")
+        .about("Encode images into jpeg xl format")
+        .long_about(indoc! {"Encode images into jpeg xl format
+
+        Only supports lossless encoding"})
 }
