8578367fdf32f17eb7fe67e891c65a0ff1c470d6
Vladyslav Vladinov <vladinov.dev@gmail.com>
2024-03-24 12:26:09 +0100

    feat(cli/help): added codecs support section

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,5 @@
 use clap::{command, Command};
+use indoc::indoc;
 
 use self::codecs::Codecs;
 
@@ -9,7 +10,29 @@ pub mod preprocessors;
 pub mod utils;
 
 pub fn cli() -> Command {
-    command!().arg_required_else_help(true).codecs()
+    command!()
+        .arg_required_else_help(true)
+        .after_help(indoc! {r#"List of supported codecs
+
+        | Image Format | Decoder       | Encoder                 |
+        | ------------ | ------------- | ----------------------- |
+        | bmp          | zune-bmp      | X                       |
+        | jpeg         | zune-jpeg     | mozjpeg or jpeg-encoder |
+        | png          | zune-png      | oxipng or zune-png      |
+        | avif         | libavif       | ravif                   |
+        | webp         | webp          | webp                    |
+        | ppm          | zune-ppm      | zune-ppm                |
+        | qoi          | zune-qoi      | zune-qoi                |
+        | farbfeld     | zune-farbfeld | zune-farbfeld           |
+        | psd          | zune-psd      | X                       |
+        | jpeg-xl      | jxl-oxide     | zune-jpegxl             |
+        | hdr          | zune-hdr      | zune-hdr                |
+
+        List of supported preprocessing options
+
+        - Resize
+        - Quantization"#})
+        .codecs()
 }
 
 #[cfg(test)]
