98728e0123fcdc9000dd0bd4bee8d9cef0b45800
Vladyslav Vladinov <vladinov.dev@gmail.com>
2024-04-11 19:45:53 +0200

    fix(codecs): updated webp decoder to new api

--- src/codecs/webp/decoder/tests.rs
+++ src/codecs/webp/decoder/tests.rs
@@ -1,19 +1,14 @@
-use std::fs::read;
-
-use zune_core::bytestream::ZByteReader;
+use std::fs::File;
 
 use super::*;
 
 #[test]
 fn decode() {
-    let file_content = read("tests/files/webp/f1t.webp").unwrap();
+    let file_content = File::open("tests/files/webp/f1t.webp").unwrap();
 
-    let reader = ZByteReader::new(file_content);
+    let mut decoder = WebPDecoder::try_new(file_content).unwrap();
 
-    let mut decoder = WebPDecoder::try_new(reader).unwrap();
-
-    let img =
-        <WebPDecoder<ZByteReader<Vec<u8>>> as DecoderTrait<Vec<u8>>>::decode(&mut decoder).unwrap();
+    let img = Image::from_decoder(decoder).unwrap();
 
     assert_eq!(img.dimensions(), (48, 80));
     assert_eq!(img.colorspace(), ColorSpace::RGBA);
