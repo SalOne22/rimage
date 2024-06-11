9733e6347e45c75bf1f88f2f10a2eb837afd7ecf
SeaEagle1 <seaeagle1@users.sourceforge.net>
2024-06-12 01:41:26 +0200

    Tiff decoding in new API

--- src/cli/pipeline.rs
+++ src/cli/pipeline.rs
@@ -1,4 +1,5 @@
 use std::{collections::BTreeMap, fs::File, io::Read, path::Path};
+use std::io::{Seek, SeekFrom};
 
 use clap::ArgMatches;
 #[cfg(feature = "avif")]
@@ -9,7 +10,10 @@ use rimage::codecs::mozjpeg::MozJpegEncoder;
 use rimage::codecs::oxipng::OxiPngEncoder;
 #[cfg(feature = "webp")]
 use rimage::codecs::webp::WebPEncoder;
+#[cfg(feature = "tiff")]
+use tiff::{ColorType, decoder::DecodingResult};
 use zune_core::{bytestream::ZByteWriterTrait, options::EncoderOptions};
+use zune_core::colorspace::ColorSpace;
 use zune_image::{
     codecs::{
         farbfeld::FarbFeldEncoder, jpeg::JpegEncoder, jpeg_xl::JxlEncoder, png::PngEncoder,
@@ -56,6 +60,34 @@ pub fn decode<P: AsRef<Path>>(f: P) -> Result<Image, ImageErrors> {
 
                 return Image::from_decoder(decoder);
             };
+
+            #[cfg(feature = "tiff")]
+            if f.as_ref()
+                .extension()
+                .is_some_and(|f| f.eq_ignore_ascii_case("tiff") | f.eq_ignore_ascii_case("tif"))
+            {
+                let mut decoder = tiff::decoder::Decoder::new(file)
+                    .map_err(|e| {ImageErrors::ImageDecodeErrors("Unable to load TIFF file - ".to_owned() + &e.to_string())})?;
+                let (width, height) = decoder.dimensions()
+                    .map_err(|_| {ImageErrors::ImageDecodeErrors("Unable to read dimensions - ".to_owned() + &e.to_string())})?;
+                let colorspace = match decoder.colortype()
+                    .map_err( |_| {ImageErrors::ImageDecodeErrors("Unable to read colorspace - ".to_owned() + &e.to_string())})? {
+                    ColorType::RGB(8) | ColorType::RGB(16) => ColorSpace::RGB,
+                    ColorType::RGBA(8) | ColorType::RGBA(16) => ColorSpace::RGBA,
+                    _ => ColorSpace::Unknown,
+                };
+
+                let decoded_data = decoder.read_image()
+                    .map_err(|_| {ImageErrors::ImageDecodeErrors("Unable to decode TIFF file - ".to_owned() + &e.to_string())})?;
+
+                return match decoded_data {
+                    DecodingResult::U8(v) => Ok(Image::from_u8(v.as_slice(),
+                                                               width as usize, height as usize, colorspace)),
+                    DecodingResult::U16(v) => Ok(Image::from_u16(v.as_slice(),
+                                                                 width as usize, height as usize, colorspace)),
+                    _ => Err(ImageErrors::ImageDecodeErrors("Tiff Data format not supported".parse().unwrap())),
+                }
+            }
 
             Err(ImageErrors::ImageDecoderNotImplemented(
                 ImageFormat::Unknown,
