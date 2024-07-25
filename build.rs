9f0ca79e4751dda6ae9e87f39b0088b659e175e4
LinkChou <linkchou@yandex.com>
2024-07-25 23:13:09 +0800

    chore: del useless

--- build.rs
+++ build.rs
@@ -12,15 +12,7 @@ fn main() {
     }
 
     let mut res = winres::WindowsResource::new();
-    // if cfg!(unix) {
-    //     // paths for X64 on archlinux
-    //     res.set_toolkit_path("/usr/x86_64-w64-mingw32/bin");
-    //     // ar tool for mingw in toolkit path
-    //     res.set_ar_path("ar");
-    //     // windres tool
-    //     res.set_windres_path("/usr/bin/x86_64-w64-mingw32-windres");
-    // }
-
+    
     //version  X. X. X. X
     //         ⇑  ⇑  ⇑
     //         48 32 16
@@ -37,5 +29,4 @@ fn main() {
         eprintln!("{}", e);
         std::process::exit(1);
     }
-    // }
 }
