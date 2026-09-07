8a577142f2e0925821b6f31e2d7171794a684b67
Mikachu2333 <linkchou@yandex.com>
2026-09-07 19:00:40 +0800

    fix(build): support CARGO_PKG_VERSION_PRE env variable

--- build.rs
+++ build.rs
@@ -17,17 +17,15 @@ fn main() {
         std::process::exit(1);
     }
 
-    let pack = |pre: u16| -> u64 {
-        (env_u64("CARGO_PKG_VERSION_MAJOR") << 48)
-            | (env_u64("CARGO_PKG_VERSION_MINOR") << 32)
-            | (env_u64("CARGO_PKG_VERSION_PATCH") << 16)
-            | u64::from(pre)
-    };
+    let packed = (env_u64("CARGO_PKG_VERSION_MAJOR") << 48)
+        | (env_u64("CARGO_PKG_VERSION_MINOR") << 32)
+        | (env_u64("CARGO_PKG_VERSION_PATCH") << 16)
+        | (env_u64("CARGO_PKG_VERSION_PRE"));
 
     let mut res = WindowsResource::new();
 
-    res.set_version_info(VersionInfo::FILEVERSION, pack(VERSION_PRE))
-        .set_version_info(VersionInfo::PRODUCTVERSION, pack(VERSION_PRE));
+    res.set_version_info(VersionInfo::FILEVERSION, packed)
+        .set_version_info(VersionInfo::PRODUCTVERSION, packed);
 
     if let Err(e) = res.compile() {
         eprintln!("{e}");
